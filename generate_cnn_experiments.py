f2bb506de21851eca71286c9b2d9b836e3e7e1a4
Beren <bmillidgework@gmail.com>
2020-05-27 20:49:21 +0100

    Add files via upload

--- generate_cnn_experiments.py
+++ generate_cnn_experiments.py
@@ -7,7 +7,7 @@ network_type = str(sys.argv[4])
 dataset = str(sys.argv[5])
 exp_name = str(sys.argv[6])
 old_savename = "None"
-base_call = "python cnn.py --network_type " + network_type + " --dataset " + dataset +" --learning_rate 0.0005"
+base_call = "python cnn.py --network_type " + network_type + " --dataset " + dataset +" --learning_rate 0.00025 --n_inference_steps 100"
 output_file = open(generated_name, "w")
 seeds = 3
 for s in range(seeds):
